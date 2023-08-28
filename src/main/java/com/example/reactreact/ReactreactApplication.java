7b4e093d96d565e17ba0a9e7537856f7c9776171
김정민 <kimjm9347@danusys.com>
2023-08-28 09:18:46 +0900

    init

---
+++ src/main/java/com/example/reactreact/ReactreactApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactreactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactreactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactreact/ReactreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactreactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
